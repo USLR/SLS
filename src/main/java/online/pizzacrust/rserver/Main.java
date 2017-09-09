281d0de8cba8b9d55bb09ab3f32b9a81d1de72d2
PizzaCrust <pizzacrustcoder@gmail.com>
2017-09-09 22:14:02 +0700

    create pom

--- src/main/java/online/pizzacrust/rserver/Main.java
+++ src/main/java/online/pizzacrust/rserver/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String... args) throws Exception {
         Server server = new Server(Integer.parseInt(args[0]));
         System.out.println("port " + args[0]);
+        System.out.println("code " + ServerRegistrationsResponse.getRegistry());
         ContextHandler getAll = new ContextHandler();
         getAll.setContextPath("/getallservers");
         getAll.setHandler(new GetAllServerHandler());
