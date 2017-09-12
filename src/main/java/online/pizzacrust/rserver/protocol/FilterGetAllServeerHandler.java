6ca474a3aed06cc3128be25d7bfa5718835b201f
PizzaCrust <pizzacrustcoder@gmail.com>
2017-09-12 18:09:20 +0700

    create pom

---
+++ src/main/java/online/pizzacrust/rserver/protocol/FilterGetAllServeerHandler.java
@@ -0,0 +1,51 @@
+package online.pizzacrust.rserver.protocol;
+
+import com.google.gson.Gson;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import online.pizzacrust.rserver.ServerRegistrationsResponse;
+
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+public class FilterGetAllServeerHandler extends AbstractHandler {
+    @Override
+    public void handle(String s, Request implRequest, HttpServletRequest apiRequest,
+                       HttpServletResponse
+            apiResponse) throws IOException, ServletException {
+        Collection<String> key = apiRequest.getParameterMap().keySet();
+        if (!key.contains("game_id")) {
+            apiResponse.setStatus(SC_BAD_REQUEST);
+            implRequest.setHandled(true);
+            return;
+        }
+        apiResponse.setContentType("application/json");
+        apiResponse.setStatus(SC_OK);
+        ServerRegistrationsResponse.ServerRegistry unFiltered = ServerRegistrationsResponse
+                .getRegistry();
+        ServerRegistrationsResponse.ServerRegistry filtered = new ServerRegistrationsResponse
+                .ServerRegistry();
+        List<ServerRegistrationsResponse> responses = new ArrayList<>();
+        int specifiedId = Integer.parseInt(apiRequest.getParameter("game_id"));
+        unFiltered.servers.forEach((r) -> {
+            if (specifiedId == r.placeId) {
+                responses.add(r);
+            }
+        });
+        filtered.servers = responses;
+        String json = new Gson().toJson(filtered.servers);
+        apiResponse.getWriter().println(json);
+        implRequest.setHandled(true);
+    }
+}
